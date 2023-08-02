a79723149be340087597a7d7e2ce17b0811d04ef
wallowitch2 <wallowitch2@gmail.com>
2023-08-02 19:18:53 +0900

    finish

--- portfolio/A. devops/devops.sql
+++ portfolio/A. devops/devops.sql
@@ -1,13 +1,2 @@
 
 CREATE DATABASE IF NOT EXISTS `Django_community`;
-USE `Django_community`;
-
-CREATE TABLE IF NOT EXISTS `Post` (
-  `no` int(11) NOT NULL,
-  `title` varchar(50) NOT NULL,
-  `content` varchar(50) NOT NULL,
-  `created_at` date NOT NULL,
-  `update_at` date NOT NULL,
-  `author` varchar(50) NOT NULL,
-  PRIMARY KEY (`no`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
